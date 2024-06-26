65911fc85312ef4bb2f4d72db6e285d28844b0b6
Arbitr05 <rosarbitr01@gmail.com>
2024-06-26 19:46:59 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,16 @@
+my_dict={'Konstantin': 1988, 'Dinara': 1988 }
+print(my_dict)
+print(my_dict['Konstantin'])
+my_dict['Kiril']=2008
+print(my_dict)
+my_dict.update({'Anton':2009,'Lera'
+    :2011})
+print(my_dict.pop('Konstantin'))
+print(my_dict)
+my_set={1,2,3,4,'patrik',True,(9,8,9)}
+print(my_set)
+my_set.update({'urban',(9,8,7)})
+my_set.discard({1,2,3,4})
+print(my_set)
+
+
